8760cde3554b91008358d3a1e7115b1d3a90d833
Paulius Kiselis <paulius.kiselis@wahanda.com>
2018-09-15 14:10:05 +0300

    fixed a gramma mistake

--- client/src/store/selectors/tile/tileSelectors.ts
+++ client/src/store/selectors/tile/tileSelectors.ts
@@ -50,15 +50,15 @@ export const getUnexploredNeighbors = (
   characterPosition: ICoordinate,
   map: ICoordinate[],
 ) :ICoordinate[] => {
-  const neigbors = getNeighborCoordinates(characterPosition);
-  return neigbors.filter((neigbor) => {
-    return !map.some(coordinate => coordinate.q === neigbor.q && coordinate.r === neigbor.r);
+  const neighbors = getNeighborCoordinates(characterPosition);
+  return neighbors.filter((neighbor) => {
+    return !map.some(coordinate => coordinate.q === neighbor.q && coordinate.r === neighbor.r);
   });
 };
 
 export const areNeighbors = (tile1: ICoordinate, tile2: ICoordinate): boolean => {
-  return getNeighborCoordinates(tile1).some((neigbor) => {
-    return neigbor.q === tile2.q && neigbor.r === tile2.r;
+  return getNeighborCoordinates(tile1).some((neighbor) => {
+    return neighbor.q === tile2.q && neighbor.r === tile2.r;
   });
 };
 
